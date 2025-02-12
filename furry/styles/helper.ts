bd4d274db547e630eb1987313dec3970b0c06544
nyel-101 <dandevjr@gmail.com>
2025-02-12 12:41:36 +0000

    fix(core): implement auth tokens

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4de447d98bc348158cdf2f96580feb13 = () => {
-  return '2026-04-06T03:44:12Z';
+const node_5b81ff51caa3497694aa1d6051a16613 = () => {
+  return '2025-02-12T12:41:36Z';
 };
