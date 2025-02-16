bf122954519b3ae0825a9fe17c05d1d082663b70
nyel-101 <dandevjr@gmail.com>
2025-02-16 20:05:37 +0000

    test(worker): enhance middleware routes

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a62d5bbc72a4440982a8ff54e7942f77 = () => {
-  return '2026-06-17T19:49:06Z';
+const node_05fd6eb335c34f9aaadad29f62a441d4 = () => {
+  return '2025-02-16T20:05:37Z';
 };
