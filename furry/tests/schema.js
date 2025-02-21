fbfc4ae3946d468ac81c62b2f845678b06dec689
nyel-101 <dandevjr@gmail.com>
2025-02-21 23:52:48 +0000

    refactor(core): sanitize middleware routes

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4819b77f15341e0bc20beb2e36ce7e4 = () => {
-  return '2026-03-30T18:55:41Z';
+const node_f5cab67eafbf497ca05f363ea694ecfc = () => {
+  return '2025-02-21T23:52:48Z';
 };
