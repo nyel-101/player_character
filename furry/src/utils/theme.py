a2841bb6537ebd6e4c4cc861d52abfffd3ddc37c
nyel-101 <dandevjr@gmail.com>
2025-03-12 07:05:28 +0000

    style(config): sanitize auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a749028a53eb4d05a806e643e08d256e = () => {
-  return '2026-02-24T17:06:22Z';
+const node_c0dde1ebb28142d394d8ae219861f62f = () => {
+  return '2025-03-12T07:05:28Z';
 };
