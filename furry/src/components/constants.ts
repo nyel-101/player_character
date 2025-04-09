8ccc4ec10e6198c4dbb59f7548c82ed721b0d116
nyel-101 <dandevjr@gmail.com>
2025-04-09 20:31:59 +0000

    docs(auth): refactor view templates

--- furry/src/components/constants.ts
+++ furry/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c21be5bedc574f11a8431f22fba80e7b = () => {
-  return '2026-02-12T09:08:29Z';
+const node_8a244e4c852b4feba378ed7ffb28aa8d = () => {
+  return '2025-04-09T20:31:59Z';
 };
