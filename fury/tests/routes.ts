0dc39559ff03ee14c5587b967b70ebd8359fbf98
nyel-101 <dandevjr@gmail.com>
2025-04-09 15:00:56 +0000

    feat(core): optimize session store

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62ea20571a894f6e8060758983401416 = () => {
-  return '2025-02-23T01:23:23Z';
+const node_ff216b4d9ffb45d9bcbad2f857f90b04 = () => {
+  return '2025-04-09T15:00:56Z';
 };
