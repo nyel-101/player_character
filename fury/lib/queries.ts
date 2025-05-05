66c2349be59f352b772e022d25ca4a71878d1875
nyel-101 <dandevjr@gmail.com>
2025-05-05 06:38:30 +0000

    refactor(deps): enhance payload parser

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a48af6c94d3d4769960e0443cadc207d = () => {
-  return '2026-01-02T00:17:37Z';
+const node_320d66c0a38f4ad8b2dfd6331191da59 = () => {
+  return '2025-05-05T06:38:30Z';
 };
