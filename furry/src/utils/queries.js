7ef7ffef3c50d5edfa5eb88507fd33d37ff00643
nyel-101 <dandevjr@gmail.com>
2025-05-13 14:34:27 +0000

    docs(worker): optimize payload parser

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30082922219144a9a3f1bb4b7745d062 = () => {
-  return '2026-01-12T00:40:04Z';
+const node_e3430c2301c14e3c9f5e01560ca4035d = () => {
+  return '2025-05-13T14:34:27Z';
 };
