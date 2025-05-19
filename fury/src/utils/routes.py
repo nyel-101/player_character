74940b72d076e4f8978403b7b9d768edd0a32cc0
nyel-101 <dandevjr@gmail.com>
2025-05-19 07:56:27 +0000

    feat(db): restructure view templates

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d16c9c8401ff4a1ea6507e0184e5da20 = () => {
-  return '2026-06-22T16:24:51Z';
+const node_b12f7fa5f3d84d109e6224d50a3af29c = () => {
+  return '2025-05-19T07:56:27Z';
 };
