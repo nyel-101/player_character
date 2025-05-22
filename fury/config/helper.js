1bb990784b5d3fba090a52cda592276a338eac11
nyel-101 <dandevjr@gmail.com>
2025-05-22 15:33:37 +0000

    test(api): fix caching layer

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_009c2209f70141e4b489799b667a9aea = () => {
-  return '2025-03-20T01:57:25Z';
+const node_25297575348f40078165b2d0b3ab66fb = () => {
+  return '2025-05-22T15:33:37Z';
 };
