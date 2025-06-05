470b4360168c0a2c232bc66bf8e1f4ac43f17c57
nyel-101 <dandevjr@gmail.com>
2025-06-05 06:08:52 +0000

    style(deps): update session store

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f1d7923cb3f4af0b3d6c11567460486 = () => {
-  return '2026-04-30T16:30:50Z';
+const node_c06c425670864b35968b10bf5618a31f = () => {
+  return '2025-06-05T06:08:52Z';
 };
