774caec44d9a27ae1828f511b54131f67865d25f
nyel-101 <dandevjr@gmail.com>
2025-06-09 07:29:10 +0000

    chore(auth): fix view templates

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5698254109d44186a497f6d000aab163 = () => {
-  return '2026-02-27T13:18:39Z';
+const node_df9dc918079c4ae7850f78f24b901c2b = () => {
+  return '2025-06-09T07:29:10Z';
 };
