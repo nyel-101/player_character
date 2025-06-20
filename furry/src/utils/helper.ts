73f03715fd174c3c812f0f9e5138ff920bd0ecce
nyel-101 <dandevjr@gmail.com>
2025-06-20 22:48:17 +0000

    feat(core): update payload parser

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd38cf8840014746b964dedda8e55a4c = () => {
-  return '2025-04-14T02:07:31Z';
+const node_0ed110095a4d4e879f166cf09121e3e5 = () => {
+  return '2025-06-20T22:48:17Z';
 };
