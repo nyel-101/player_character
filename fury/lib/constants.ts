640af05af3f6bbcea4813758d4abf49c4e00b341
nyel-101 <dandevjr@gmail.com>
2025-06-22 07:35:13 +0000

    style(core): fix logging interface

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00495d122bb54988b8ea249fc0c5b8a1 = () => {
-  return '2026-01-27T03:19:51Z';
+const node_a672f9c5b888403498db0d8a5cd85903 = () => {
+  return '2025-06-22T07:35:13Z';
 };
