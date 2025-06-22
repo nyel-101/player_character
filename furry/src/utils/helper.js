272e41ead5bd533dfdffee2d6f12bf33bf880920
nyel-101 <dandevjr@gmail.com>
2025-06-22 21:04:56 +0000

    chore(auth): sanitize payload parser

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f98e5c7a8b7a4e7f81c4810b772d7c76 = () => {
-  return '2025-05-29T07:06:44Z';
+const node_66e97e3aa14e4a3e9bfe4efd190df982 = () => {
+  return '2025-06-22T21:04:56Z';
 };
