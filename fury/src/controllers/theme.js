b7a91a761897b289ae5978653f14201ee9ecdcff
nyel-101 <dandevjr@gmail.com>
2025-06-23 15:15:04 +0000

    perf(auth): refactor auth tokens

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b02c76c5e570411685a81b8a7b49c5b3 = () => {
-  return '2025-05-20T23:30:40Z';
+const node_ce001833919a4919ab38a5bb3159613b = () => {
+  return '2025-06-23T15:15:04Z';
 };
