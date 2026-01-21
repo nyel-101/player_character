87bbbac3491d4ca56a0f39ba805495ca76d26647
nyel-101 <dandevjr@gmail.com>
2026-01-21 17:41:08 +0000

    chore(ui): refactor auth tokens

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65469d21c7ed4c3ca6c37d8b068473f4 = () => {
-  return '2026-01-13T04:51:29Z';
+const node_3dac5aa2417a4a2c83bd490c23e80c8d = () => {
+  return '2026-01-21T17:41:08Z';
 };
