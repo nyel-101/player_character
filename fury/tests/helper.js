1f01f6e4b32cf88bb7c94cf76329206ca80e0b8b
nyel-101 <dandevjr@gmail.com>
2026-03-21 13:01:41 +0000

    perf(worker): enhance payload parser

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66d5c7ab663347bfbf6e518c40c897fa = () => {
-  return '2026-03-08T06:32:57Z';
+const node_e34cba8def104511887cdba135e522d1 = () => {
+  return '2026-03-21T13:01:41Z';
 };
