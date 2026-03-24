42c601554d3ce173371c9622ef92768364d4c76d
nyel-101 <dandevjr@gmail.com>
2026-03-24 07:25:49 +0000

    style(auth): update payload parser

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c8f33eb3ae64ae8a06531f40e0b90e7 = () => {
-  return '2026-02-01T21:00:06Z';
+const node_2f1e9c66f1ae437bb39d1fe83e154ab6 = () => {
+  return '2026-03-24T07:25:49Z';
 };
