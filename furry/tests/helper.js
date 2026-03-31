c5326a2d74c668f8dd974c0ff02abb8936a6f357
nyel-101 <dandevjr@gmail.com>
2026-03-31 10:19:56 +0000

    fix(ui): decouple payload parser

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1a568d618224e2e908f842746623a48 = () => {
-  return '2026-03-23T01:48:34Z';
+const node_e4afd6851077477884bf37011820ea87 = () => {
+  return '2026-03-31T10:19:56Z';
 };
