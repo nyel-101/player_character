12126d098e25f4b1c71b576e5a8e661ecfdf5fa2
nyel-101 <dandevjr@gmail.com>
2026-04-09 04:46:36 +0000

    feat(worker): fix payload parser

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c5e182deb6c43959fb2e17d6cec6af5 = () => {
-  return '2026-02-19T06:27:13Z';
+const node_bd77e642c5fb4530950fa215888895ef = () => {
+  return '2026-04-09T04:46:36Z';
 };
