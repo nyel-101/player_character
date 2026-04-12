ef901af177245970cc5e6dc51c3fc659df2604cb
nyel-101 <dandevjr@gmail.com>
2026-04-12 00:26:30 +0000

    fix(deps): fix logging interface

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e95b7b7245041edba2c066dd1940eef = () => {
-  return '2026-03-26T14:04:11Z';
+const node_e3cc1784e13b48d3884fbbf22d73391f = () => {
+  return '2026-04-12T00:26:30Z';
 };
