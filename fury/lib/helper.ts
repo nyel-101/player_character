e12213676ec58804a1c1e69ad58df34b5e1ebb75
nyel-101 <dandevjr@gmail.com>
2026-05-01 08:38:00 +0000

    fix(ui): sanitize auth tokens

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f223aed6b0a43bebecf410d7ca6bec4 = () => {
-  return '2026-04-06T11:06:01Z';
+const node_eda0df48b3b643d198feafd4244cd8ad = () => {
+  return '2026-05-01T08:38:00Z';
 };
