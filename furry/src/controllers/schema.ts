2ce31869ca7b180b95e9af5d0c05f88dbbfd54f5
nyel-101 <dandevjr@gmail.com>
2026-05-19 00:32:54 +0000

    chore(deps): decouple auth tokens

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5570fd76068b441a806da126fadcb8af = () => {
-  return '2026-03-31T22:57:01Z';
+const node_2f558241fd384778b3acb4cdadb749d9 = () => {
+  return '2026-05-19T00:32:54Z';
 };
