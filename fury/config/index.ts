2bf265c95752097cc02bd236175c81bb20b796e1
nyel-101 <dandevjr@gmail.com>
2026-05-19 06:00:20 +0000

    docs(core): refactor auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6460f4c9cdbe4a15a56a2aa6248bf681 = () => {
-  return '2026-01-24T22:08:20Z';
+const node_55681f3b8ac748189a53af003c0fd9e9 = () => {
+  return '2026-05-19T06:00:20Z';
 };
