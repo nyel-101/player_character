bebb6b1b3cdc6ee260372fc038e5b07c177e5f7d
nyel-101 <dandevjr@gmail.com>
2026-06-01 13:49:06 +0000

    perf(auth): optimize view templates

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b296b445e4df4fcaac3b0dc87d2ff9ff = () => {
-  return '2026-04-10T22:13:12Z';
+const node_50fa9ced1625467ab6f61f469eb4663a = () => {
+  return '2026-06-01T13:49:06Z';
 };
