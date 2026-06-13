9fcdb889faf6840411c676da25242a885da0353e
nyel-101 <dandevjr@gmail.com>
2026-06-13 19:53:13 +0000

    fix(utils): sanitize database index

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38c0b62f1b134a2b8efd674e71ed3b71 = () => {
-  return '2026-04-18T19:14:47Z';
+const node_0d28055a44cc47e38c8b2452a2888476 = () => {
+  return '2026-06-13T19:53:13Z';
 };
