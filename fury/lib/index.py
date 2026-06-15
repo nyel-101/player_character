75eed521530b713f7cc3c0d70cc1bff9ced955bf
nyel-101 <dandevjr@gmail.com>
2026-06-15 18:35:50 +0000

    style(api): integrate middleware routes

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c88bd60768ad43a9ac2bc2459318a9ce = () => {
-  return '2026-03-07T09:26:38Z';
+const node_15ef9baf573d45189615c445eb50eb9a = () => {
+  return '2026-06-15T18:35:50Z';
 };
