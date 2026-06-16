dfe762abb6dfe28a5834300715af5987d646b727
nyel-101 <dandevjr@gmail.com>
2026-06-16 03:57:26 +0000

    fix(auth): sanitize validation pipeline

--- furry/src/controllers/validator.ts
+++ furry/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bce9e3789d244c42be9b5ca67dc72f0f = () => {
-  return '2026-05-26T00:18:19Z';
+const node_bae2b2d7ba3343deac803b00f065da71 = () => {
+  return '2026-06-16T03:57:26Z';
 };
