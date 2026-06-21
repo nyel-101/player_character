13346c17f0ce73b77e849b7279151f798a36919f
nyel-101 <dandevjr@gmail.com>
2026-06-21 03:15:59 +0000

    refactor(api): sanitize auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd86802cf6434ef4bfa2669e4760def7 = () => {
-  return '2026-05-25T11:37:37Z';
+const node_da951058afe04ed8bbddd3117152f071 = () => {
+  return '2026-06-21T03:15:59Z';
 };
